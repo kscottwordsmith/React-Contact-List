769e83236e48d203435c384c4c46ef11e10ffbec
kscottwordsmith <kscottwordsmith@gmail.com>
2018-12-27 16:19:10 -0800

    pretty much done?

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Users from './UserContent.json'
 import { BrowserRouter as Router } from 'react-router-dom'
+import 'font-awesome/css/font-awesome.css'
+import './Contact.css'
 
 class Contact extends Component {
     state = {
@@ -20,7 +22,7 @@ class Contact extends Component {
             return per.id == this.props.match.params.id
         })
         this.setState({
-            img: person.picture.thumbnail,
+            img: person.picture.large,
             fname: person.name.first,
             lname: person.name.last,
             email: person.email,
@@ -31,19 +33,16 @@ class Contact extends Component {
     }
     render() {
         return (
-        <Router>
             <div>
-                <button id="backbutton" onClick={this.backButton}>Back</button>
-                <ul>
-                    <li><img src={this.state.img} alt={this.state.fname + " Image"} /></li>
-                    <li>{this.state.fname} {this.state.lname}</li>
-                    <li>{this.state.email}</li>
-                    <li>{this.state.cell}</li>
-                    <li>{this.state.city}</li>
-                    <li>{this.state.st}</li>
+                <button id="backbutton" onClick={this.backButton}><i className="fa fa-arrow-left"></i></button>
+                <ul id="contactInfo">
+                    <li id="contactImg"><img src={this.state.img} alt={this.state.fname + " Image"} /></li>
+                    <li className="needsCaps contactText"><i class="fa fa-user"></i> {this.state.fname} {this.state.lname}</li>
+                    <li className="contactText"><i className="fa fa-envelope"></i> {this.state.email}</li>
+                    <li className="contactText"><i className="fa fa-mobile"></i> {this.state.cell}</li>
+                    <li className="needsCaps contactText"><i className="fa fa-building"></i> {this.state.city}, {this.state.st}</li>
                 </ul>
             </div>
-        </Router>
         )
     }
 }
--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react'
 import Users from './UserContent.json'
 import { BrowserRouter as Router, Link } from 'react-router-dom'
+import './Home.css'
 
 const nameArr = Users.map((user) => (
-    <li key={'peeps-name-list' + user.id}>
+    <li key={'peeps-name-list' + user.id} className="peepname">
       <Link to={"/Contact/" + user.id}>
-        {capitalizeFirstLetter(user.name.first)} {capitalizeFirstLetter(user.name.last)}
+        <img src={user.picture.thumbnail} /> {user.name.first} {user.name.last}
       </Link>
     </li>
   ))
-  
-  function capitalizeFirstLetter(name) {
-    return name.charAt(0).toUpperCase() + name.slice(1)
-  }
 
 class Home extends Component {
     render() {
         return (
             <div className="App">
-                <h1 id="listViewHead">My Peeps</h1>
+                <div id="listViewHead">My Peeps</div>
                 <ul id="peepsList">
                     {nameArr}
                 </ul>
